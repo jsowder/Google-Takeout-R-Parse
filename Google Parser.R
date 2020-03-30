5da07b4f6af7ada155e863eb544e48bab17b870a
Jacob Sowder <jacobsowder@gmail.com>
2020-03-30 11:36:36 -0400

    Add comments

--- Google Parser.R
+++ Google Parser.R
@@ -1,27 +1,54 @@
+# Import Libraries
 library(tidyverse)
 library(rvest)
 library(xml2)
 library(lubridate)
 
-# Clean it up function
+# Before starting, download your Google Takeout file from:
+# https://takeout.google.com/
+
+#########################
+# DEFINE YOUR VARIABLES #
+#########################
+
+# Where did you download your Takeout files to?
+takeout_download_path <- "~/Downloads/Takeout"
+
+# Where would you like to save a CSV of your results?
+resulting_csv_path <- "example.csv"
+
+# Now run the script!
+# If you add anything helpful please pull and commit -- thanks!
+# https://github.com/jsowder/Google-Takeout-R-Parse
+
+#########################^
+# DEFINE YOUR VARIABLES #^
+#########################^
+
+# General function for cleaning HTML files present in Google Takeout
+# Each variable explained with a comment where it is created
 clean_takeout_html <- function(file_path){
   r <-
     read_html(file_path) %>%
-    html_nodes(".outer-cell") %>% 
+    html_nodes(".outer-cell") %>% # Gather each card you see in the HTML file
     tibble() %>% 
     rename("raw" = ".") %>% 
     mutate(Service = "GoogleTakeout",
            
+           # Search/Youtube/Maps/etc (TITLE of the card)
            ServiceDetail = raw %>% 
              html_node(".header-cell") %>% 
              html_text(),
            
+           # Timestamp in the card
            StartTime = raw %>% 
              str_extract(
                pattern = "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [:digit:]{1,2}, [:digit:]{4}, [:digit:]{1,2}:[:digit:]{1,2}:[:digit:]{1,2} ([AP]M) EDT"
                ) %>% 
              parse_datetime(format = "%b %d, %Y, %T %p %Z"),
            
+           # Whatever comes before the colon/etc -- mine is miscoded as the Â character, change as needed.
+           #  e.g. "Searched for:", "Visited:", "Got directions to:", etc.
            Activity = raw %>%
              html_node(css = ".content-cell") %>%
              as_list() %>% 
@@ -29,12 +56,16 @@ clean_takeout_html <- function(file_path){
              str_remove("Â") %>% 
              str_trim(),
            
+           # Text contents of the link following the colon
+           #   e.g. video watched, text searched, address of directions
            Detail = raw %>% 
              html_node(css = ".content-cell") %>%
              html_node("a") %>% 
              html_text() %>% 
              str_trim(),
            
+           # Link to whatever followed the colon
+           #   e.g. google.com/query=SearchContents...
            Link = raw %>% 
              html_node(css = ".content-cell") %>% 
              html_node("a") %>% 
@@ -47,7 +78,6 @@ clean_takeout_html <- function(file_path){
 
 
 # Import data
-takeout_download_path <- "/Users/jacobsowder/Library/Mobile Documents/com~apple~CloudDocs/Data_Mine/Google/_mar-26-2020/Takeout"
 file_paths <- 
   c(list.files(path = takeout_download_path, recursive = TRUE, full.names = TRUE, pattern = "MyActivity|history.html")) 
 
@@ -63,11 +93,9 @@ all_data <-
   arrange(desc(StartTime)) %>% 
   distinct()
 
-# Final touches
-# all_data %<>% 
 
 # Export Clean Data
 write_csv(all_data,
-          path = "/Users/jacobsowder/Library/Mobile Documents/com~apple~CloudDocs/Data_Mine/_new/Time/google_takeout_time.csv",
+          path = resulting_csv_path,
           append = FALSE)
 
